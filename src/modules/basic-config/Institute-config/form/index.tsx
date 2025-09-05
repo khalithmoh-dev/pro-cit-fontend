5722ab21a523dc630f4bf585356449037b4cd250
khalithmoh <khalithmoh@octoze.com>
2025-09-05 20:55:47 +0530

    institute completed

--- src/modules/basic-config/Institute-config/form/index.tsx
+++ src/modules/basic-config/Institute-config/form/index.tsx
@@ -4,241 +4,237 @@ import * as Yup from "yup";
 import useInstituteStore from "../../../../store/instituteStore";
 import useBaseStore from '../../../../store/baseStore';
 import useAuthStore from '../../../../store/authStore'
-
-const schema = {
-  fields: {
-      General: [
-        {
-          name: "Insname",
-          label: "Institution name",
-          type: "text",
-          validation: Yup.string().required("Institution Name is required"),
-          isRequired: true,
-          isDisabled: true
-        },
-        {
-          name: "insCode",
-          label: "Institution code",
-          type: "text",
-          validation: Yup.string().required("Institution code is required"),
-          isRequired: true
-        },
-        { name: "insLogo", label: "Institute Logo", type: "file", size: '' ,format: 'image', isMulti: true },
-        {
-          name: "acrtdBy",
-          label: "Accredited By",
-          type: "text"
-        },
-        {
-          name: 'insDesc',
-          label: "Instiution description",
-          type: "Textarea",
-        }
-  ],
-  "Institution contact": [
-        {
-          name: "addr1",
-          label: "Address Line 1",
-          type: "text"
-        },
-       {
-          name: "addr2",
-          label: "Address Line 2",
-          type: "text"
-        },
-         {
-          name: "addr3",
-          label: "Address Line 3",
-          type: "text"
-        },
-         {
-          name: "pnCd",
-          label: "Pincode",
-          type: "number"
-        },
-        {
-          name: "cntctNum",
-          label: "Contact number",
-          type: "number"
-        },
-         {
-          name: "email",
-          label: "Email",
-          type: "text"
-        },
-         {
-          name: "webSt",
-          label: "Website",
-          type: "text"
-        },
-
-  ],
-  "Billing department contact": [
-        {
-          name: "billCntctNm",
-          label: "Contact person name",
-          type: "text"
-        }, {
-          name: "billCntctNum",
-          label: "Phone number",
-          type: "text"
-        }, {
-          name: "billCntctMail",
-          label: "Email",
-          type: "text"
-        },
-  ],
-  "Admission department contact": [
-        {
-          name: "admCntctNm",
-          label: "Contact person name",
-          type: "text"
-        }, {
-          name: "admCntctNum",
-          label: "Phone number",
-          type: "text"
-        }, {
-          name: "admCntctMail",
-          label: "Email",
-          type: "text"
-        },
-  ],
-  "Examination department contact": [
-        {
-          name: "xmCntctNm",
-          label: "Contact person name",
-          type: "text"
-        }, {
-          name: "xmCntctNum",
-          label: "Phone number",
-          type: "text"
-        }, {
-          name: "xmCntctMail",
-          label: "Email",
-          type: "text"
-        },
-  ],
-    "General enquiry": [
-        {
-          name: "gnCntctNm",
-          label: "Contact person name",
-          type: "text"
-        }, {
-          name: "gnCntctNum",
-          label: "Phone number",
-          type: "text"
-        }, {
-          name: "gnCntctMail",
-          label: "Email",
-          type: "text"
-        },
-  ],
-  "Working days": [
-    {
-      name: 'wrkDys',
-      isMulti: true,
-      type: "checkbox",
-      data: [ 
-        { label: "Sunday", value: "SUNDAY" },
-        {  label: "Monday", value: "MONDAY"},
-        {  label: "Tuesday", value: "TUESDAY"},
-        {  label: "Wednesday", value: "WEDNESDAY"},
-        {  label: "Thursday", value: "THURSDAY"},
-        {  label: "Friday", value: "FRIDAY"},
-        {  label: "Saturday", value: "SATURDAY"},]
-    }
-  ],
-  "SMS config": [
-    {
-          name: "smsUrl",
-          label: "Url",
-          type: "text"
-        },{
-          name: "smsPwd",
-          label: "Password",
-          type: "text"
-        },{
-          name: "smsUsrId",
-          label: "User Id",
-          type: "text"
-        },{
-          name: "smsSndrId",
-          label: "Sender Id",
-          type: "text"
-        },{
-          name: "smsApiKey",
-          label: "API-key",
-          type: "text"
-        },
-  ],
-  "Absent notification": [
-  {
-    name: 'isAbsNtf',
-    validation: Yup.boolean(),
-    label: "Can send absent notification",
-    type: "checkbox",
-    removeHeader: true,
-  },
-  {
-    name: 'absntTmplt',
-    label: "Absent notification template",
-    type: "Textarea",
-    showWhen: {
-      field: "isAbsNtf",
-      value: true,
-    },
-    validation: Yup.string().when('isAbsNtf', {
-      is: true,
-      then: ()=>Yup.string().required('Absent notification template is required'),
-      otherwise: ()=>Yup.string(),
-    }),
-    isRequired: true
-  }
-],
-
-  "Birthday messege": [
-        {
-          name:"isBdayMsg",
-          label: "Can Send Birthday Messege",
-          type: "checkbox",
-          removeHeader: true
-        },
-        {
-          name: "bDayTmplt",
-          label: "Birthday messege template",
-          type: "Textarea",
-          showWhen: {
-            field: "isBdayMsg",
-            value: true, 
-          },
-          validation: Yup.string().when('isBdayMsg', {
-            is: true,
-            then: ()=>Yup.string().required('Birthday messege template is required'),
-            otherwise: ()=>Yup.string(),
-          }),
-          isRequired: true
-        }
-  ]
-  },
-  buttons:[
-    {
-      name:"Cancel", variant:"outlined", color:"secondary", onClick:()=>{}
-    },{
-      name:"Reset", variant:"outlined", color:"warning", onClick:()=>{}
-    },{
-      name:"Save", variant:"contained", color:"primary", type:'submit'
-    },{
-      name:"Next", variant:"contained", color:"primary", onClick:()=>{}
-    }
-  ]
-};
-
+import { useNavigate } from "react-router-dom";
 
 export default function InstiteConfig() {
   const { createInstitute, getInstitute } = useInstituteStore();
   const { user, permissions } = useAuthStore();
   const { parseFormDataAndUpload, upldedRec } = useBaseStore()
   const [instDtls, setInstDtls] = useState({});
+  const navigate = useNavigate();
+  const schema = {
+    fields: {
+        General: [
+          {
+            name: "Insname",
+            label: "Institution name",
+            type: "text",
+            validation: Yup.string().required("Institution Name is required"),
+            isRequired: true,
+            isDisabled: true
+          },
+          {
+            name: "insCode",
+            label: "Institution code",
+            type: "text",
+            validation: Yup.string().required("Institution code is required"),
+            isRequired: true
+          },
+          { name: "insLogo", label: "Institute Logo", type: "file", size: '' ,format: 'image', isMulti: true },
+          {
+            name: "acrtdBy",
+            label: "Accredited By",
+            type: "text"
+          },
+          {
+            name: 'insDesc',
+            label: "Instiution description",
+            type: "Textarea",
+          }
+    ],
+    "Institution contact": [
+          {
+            name: "addr1",
+            label: "Address Line 1",
+            type: "text"
+          },
+        {
+            name: "addr2",
+            label: "Address Line 2",
+            type: "text"
+          },
+          {
+            name: "addr3",
+            label: "Address Line 3",
+            type: "text"
+          },
+          {
+            name: "pnCd",
+            label: "Pincode",
+            type: "number"
+          },
+          {
+            name: "cntctNum",
+            label: "Contact number",
+            type: "number"
+          },
+          {
+            name: "email",
+            label: "Email",
+            type: "text"
+          },
+          {
+            name: "webSt",
+            label: "Website",
+            type: "text"
+          },
+
+    ],
+    "Billing department contact": [
+          {
+            name: "billCntctNm",
+            label: "Contact person name",
+            type: "text"
+          }, {
+            name: "billCntctNum",
+            label: "Phone number",
+            type: "text"
+          }, {
+            name: "billCntctMail",
+            label: "Email",
+            type: "text"
+          },
+    ],
+    "Admission department contact": [
+          {
+            name: "admCntctNm",
+            label: "Contact person name",
+            type: "text"
+          }, {
+            name: "admCntctNum",
+            label: "Phone number",
+            type: "text"
+          }, {
+            name: "admCntctMail",
+            label: "Email",
+            type: "text"
+          },
+    ],
+    "Examination department contact": [
+          {
+            name: "xmCntctNm",
+            label: "Contact person name",
+            type: "text"
+          }, {
+            name: "xmCntctNum",
+            label: "Phone number",
+            type: "text"
+          }, {
+            name: "xmCntctMail",
+            label: "Email",
+            type: "text"
+          },
+    ],
+      "General enquiry": [
+          {
+            name: "gnCntctNm",
+            label: "Contact person name",
+            type: "text"
+          }, {
+            name: "gnCntctNum",
+            label: "Phone number",
+            type: "text"
+          }, {
+            name: "gnCntctMail",
+            label: "Email",
+            type: "text"
+          },
+    ],
+    "Working days": [
+      {
+        name: 'wrkDys',
+        isMulti: true,
+        type: "checkbox",
+        data: [ 
+          { label: "Sunday", value: "SUNDAY" },
+          {  label: "Monday", value: "MONDAY"},
+          {  label: "Tuesday", value: "TUESDAY"},
+          {  label: "Wednesday", value: "WEDNESDAY"},
+          {  label: "Thursday", value: "THURSDAY"},
+          {  label: "Friday", value: "FRIDAY"},
+          {  label: "Saturday", value: "SATURDAY"},]
+      }
+    ],
+    "SMS config": [
+      {
+            name: "smsUrl",
+            label: "Url",
+            type: "text"
+          },{
+            name: "smsPwd",
+            label: "Password",
+            type: "text"
+          },{
+            name: "smsUsrId",
+            label: "User Id",
+            type: "text"
+          },{
+            name: "smsSndrId",
+            label: "Sender Id",
+            type: "text"
+          },{
+            name: "smsApiKey",
+            label: "API-key",
+            type: "text"
+          },
+    ],
+    "Absent notification": [
+    {
+      name: 'isAbsNtf',
+      validation: Yup.boolean(),
+      label: "Can send absent notification",
+      type: "checkbox",
+      removeHeader: true,
+    },
+    {
+      name: 'absntTmplt',
+      label: "Absent notification template",
+      type: "Textarea",
+      showWhen: {
+        field: "isAbsNtf",
+        value: true,
+      },
+      validation: Yup.string().when('isAbsNtf', {
+        is: true,
+        then: ()=>Yup.string().required('Absent notification template is required'),
+        otherwise: ()=>Yup.string(),
+      }),
+      isRequired: true
+    }
+  ],
+
+    "Birthday messege": [
+          {
+            name:"isBdayMsg",
+            label: "Can Send Birthday Messege",
+            type: "checkbox",
+            removeHeader: true
+          },
+          {
+            name: "bDayTmplt",
+            label: "Birthday messege template",
+            type: "Textarea",
+            showWhen: {
+              field: "isBdayMsg",
+              value: true, 
+            },
+            validation: Yup.string().when('isBdayMsg', {
+              is: true,
+              then: ()=>Yup.string().required('Birthday messege template is required'),
+              otherwise: ()=>Yup.string(),
+            }),
+            isRequired: true
+          }
+    ]
+    },
+    buttons:[
+      {
+        name:"Cancel", variant:"outlined", color:"secondary", onClick:()=>{navigate(-1)}
+      },{
+        name:"Update", variant:"contained", color:"primary", type:'submit'
+      }
+    ]
+  };
 
   useEffect(()=>{
     if(user && getInstitute){
---
+++ src/store/instituteStore.ts
@@ -0,0 +1,82 @@
+import { create } from 'zustand';
+import httpRequest from '../utils/functions/http-request';
+interface createInstitutePayload{
+    Insname: string,
+    insCode: string,
+    insLogo?: string,
+    acrtdBy?: string,
+    insDesc?: string,
+    addr1?: string,
+    addr2?: string,
+    addr3?: string,
+    pnCd?: string,
+    cntctNum?: string,
+    email?: string,
+    webSt?: string,
+    billCntctNm?: string,
+    billCntctNum?: string,
+    billCntctMail?: string,
+    admCntctNm?: string,
+    admCntctNum?: string,
+    admCntctMail?: string,
+    xmCntctNm?: string,
+    xmCntctNum?: string,
+    xmCntctMail?: string,
+    gnCntctNm?: string,
+    gnCntctNum?: string,
+    gnCntctMail?: string,
+    wrkDys?: string[],
+    smsUrl?: string,
+    smsPwd?: string,
+    smsUsrId?: string,
+    smsSndrId?: string,
+    smsApiKey?: string,
+    isAbsNtf?: boolean,
+    absntTmplt?: string,
+    bDayTmplt?: boolean,
+    isBdayMsg?: string
+}
+
+interface InstituteState {
+    insData: object,
+    createInstitute: (payload: createInstitutePayload) => Promise<boolean>,
+    getInstitute: (id: string) => Promise<boolean>,
+    updateInstitute: (payload: createInstitutePayload) => Promise<boolean>
+}
+
+// Institute Store to handle institution create update get functionalities
+const useInstituteStore = create<InstituteState>((set,get) => ({
+    insData: {},
+    createInstitute: async(oPaylaod = {
+        Insname: '',
+        insCode: ''
+    }) => {
+        try{
+            const {data} = await httpRequest('POST',`${import.meta.env.VITE_API_URL}/institute/create`,oPaylaod);
+            get().getInstitute(data?._id);
+            return true
+        }catch(err){
+            return false
+        }
+    },
+    getInstitute: async(id = '') => {
+       try{
+         return (await httpRequest('GET',`${import.meta.env.VITE_API_URL}/institute/get-by-id/${'68ae0a790bfe5e87f234fbd3'}`))?.data;
+       }catch(err){
+        return false
+       }
+    },
+    updateInstitute: async(oPaylaod = {
+        Insname: '',
+        insCode: ''
+    }) => {
+        try{
+            const {data} = await httpRequest('POST',`${import.meta.env.VITE_API_URL}/institute/update`,oPaylaod);
+            get().getInstitute(data?._id);
+            return true
+        }catch(err){
+            return false
+        }
+    }
+}))
+export default useInstituteStore;
