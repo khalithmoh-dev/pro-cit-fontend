07db221ef4d108640405b6fef300110f34edfb61
khalithmoh <khalithmoh@octoze.com>
2025-10-21 21:09:37 +0530

    header translation added

--- src/components/SectionHeader/index.tsx
+++ src/components/SectionHeader/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import Typography from '@mui/material/Typography'
 import './index.css'
+import { useTranslation } from 'react-i18next';
 interface SectionHeaderProps {
   sectionName: string;
 }
 
 const SectionHeader: React.FC<SectionHeaderProps> = ({ sectionName }) => {
+  const { t } = useTranslation();
   return (
     <>
       <Typography className='section-header'>
-        {sectionName}
+        {t(sectionName)}
       </Typography>
       <hr className='m-0 p-0' style={{ color: 'black' }} />
     </>
